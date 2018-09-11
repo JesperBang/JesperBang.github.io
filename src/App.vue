61914b6be2b847c8239101e34dee1c8d2dfb9497
MrKimberwood <jesper.xenia@gmail.com>
2018-09-11 17:07:50 +0200

    build

--- src/App.vue
+++ src/App.vue
@@ -54,7 +54,7 @@
             <li class="list-inline-item">
               <a href="#">
                 <span class="fa-stack fa-lg">
-                  <i class="fa fa-circle fa-stack-2x"></i>
+                  <i class="fa fa-circle  fa-stack-2x"></i>
                   <i class="fa fa-facebook fa-stack-1x fa-inverse"></i>
                 </span>
               </a>
